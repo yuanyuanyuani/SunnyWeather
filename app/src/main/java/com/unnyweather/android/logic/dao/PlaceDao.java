359352bbc347cf2c62e844cde6c6fd0794ae7f11
Adang <1768913313@qq.com>
2021-01-05 09:28:09 +0800

    First commit.

---
+++ app/src/main/java/com/unnyweather/android/logic/dao/PlaceDao.java
@@ -0,0 +1,15 @@
+package com.unnyweather.android.logic.dao;
+
+
+import com.unnyweather.android.logic.model.Place;
+
+public class PlaceDao {
+    void savePlace(Place place)
+    {
+
+    }
+
+    public  Place getSavedPlace(){
+
+    }
+}
---
+++ app/src/main/java/com/unnyweather/android/logic/dao/place.kt
@@ -0,0 +1,7 @@
+package com.unnyweather.android.logic.dao
+
+object Place{
+    fun savePlace(){
+        
+    }
+}
